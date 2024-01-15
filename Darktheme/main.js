49e5da87409f0a345353a2438735d56eb90a293b
esthonoles <aqilallzero@gmail.com>
2024-01-15 10:52:25 +0700

    delete modul

---
+++ Darktheme/main.js
@@ -0,0 +1,20 @@
+    let body = document.body
+    let btn = document.getElementById('button')
+    let title = document.querySelector('.title')
+    let container = document.querySelector('.container')
+
+    const containerDark =`
+    
+    `
+
+function light(){
+    
+    body.classList.toggle("light-mode")
+    title.style.color = "black"
+    container.style.border = "black"
+    // btn.innerHTML = "Dark Theme"
+
+
+
+
+}
---
+++ SMT1/local-storage/tugas local-storage/main.js
@@ -0,0 +1,77 @@
+/*
+        // adding style from Js
+
+        let root = document.getElementById('root');
+        const title = "local storage session";
+
+        root.textContent = title;
+        root.style.backgroundColor = '#e06a6a';
+        root.style.color = 'white';
+        root.style.fontSize = '2em';
+        root.style.textAlign = 'center';
+        */
+
+        let username = document.getElementById('username-input');
+        let password = document.getElementById('password-input');
+        let btnLogin = document.getElementById('btnlogin');
+        let btnLogout = document.getElementById('btnlogout')
+
+        
+
+        const admin = document.getElementById('admin');
+        const user = document.getElementById('user-biasa');
+
+        btnLogout.style.display = "none"
+        admin.style.display = "none"
+        user.style.display = "none"
+        
+       
+        console.log(localStorage.getItem('username'))
+     
+     
+
+        function onLogin(){
+            localStorage.setItem('username', username.value);
+          
+            if(username.value == '' && password.value == '' || password.value==''){
+                alert('masukkan username dan password')
+            }else if(username.value == 'admin' && password.value == 'admin'){
+                localStorage.setItem('role', 'admin');
+                admin.style.display = "block"
+                user.style.display = "none"
+                }else{
+                    localStorage.setItem('role', 'basic');
+                    user.style.display = "block"
+                    admin.style.display = "none"
+                }
+            }
+        
+        if(localStorage.getItem('username')){
+           username.style.display = "none"
+           password.style.display = "none"
+           btnLogin.style.display = "none"
+           btnLogout.style.display = "block"
+           if(localStorage.getItem('role') == 'admin'){
+            admin.style.display = "block"
+            user.style.display = "none"
+           }else{
+            user.style.display = "block"
+            admin.style.display = "none"
+           }
+        }
+
+        function onLogout(){
+            localStorage.clear();
+            location.reload()   
+        }
+            
+            
+        
+
+
+
+
+
+        
+        
+        
